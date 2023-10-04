4abd69f1e071aff647710b871a7771d1c98c6bf3
KUNG HSIU TING <62473938+Timo-Kung@users.noreply.github.com>
2023-10-04 14:17:55 +0800

    Add files via upload

--- database/add_new_program_act.php
+++ database/add_new_program_act.php
@@ -3,19 +3,50 @@ session_start();
 //連接資料庫
 //只要此頁面上有用到連接MySQL就要include它
 include("sql_connect.php");
-$Date = $_POST['Date'];
-$Activity_name = $_POST['Activity_name'];
-$Activity_category = $_POST['Activity_category'];
-$Person = $_POST['Person'];
-$Location = $_POST['Location'];
-$Service = $_POST['Service'];
-$Cost = $_POST['Cost'];
-$Number = $_POST['Number'];
-$Lecturer = $_POST['Lecturer'];
+$Date = $_REQUEST['Date'];
+$Activity_name = $_REQUEST['Activity_name'];
+$Activity_category = $_REQUEST['Activity_category'];
+$Person = $_REQUEST['Person'];
+$Location = $_REQUEST['Location'];
+$Service = $_REQUEST['Service'];
+$Cost = $_REQUEST['Cost'];
+$Number = $_REQUEST['Number'];
+$Lecturer = $_REQUEST['Lecturer'];
 
 
 
 $user = $_SESSION['name'];
+
+// 上傳報表路徑
+@$file_dir = "../program_act/";
+@$file_name = "";
+@$file = "";
+
+
+// 無該檔案資料夾則建立
+if (!is_dir($file_dir)) {
+    mkdir($file_dir, 0777, true);
+}
+
+// 判斷報表上傳
+if (isset($_FILES["program_act_files0"]))
+{
+    @$file_name = $_FILES["program_act_files0"]["name"];
+    @$file = "../program_act/" . $_FILES["program_act_files0"]["name"];
+
+    
+
+    if ($_FILES["program_act_files0"]["error"] > 0) {
+
+        echo false;
+    } else {
+        //設定檔案上傳路徑，選擇指定資料夾
+        move_uploaded_file(
+            $_FILES["program_act_files0"]["tmp_name"],
+            "../program_act/" . $_FILES["program_act_files0"]["name"]
+        );
+    }
+}
 
 
 $sql = "INSERT INTO `program_act` (`Date`,`Activity_name`,`Activity_category`,`Person`,`Location`,`Service`,
