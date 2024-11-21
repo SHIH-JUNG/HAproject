5d21cbdc2a3ccb3f30bdd1d1d23cb711cbbdf4bd
Wilson314 <wliu30192031411@gmail.com>
2024-11-21 09:23:36 +0800

    version20241121_new

--- add_peers_dlgrec.php
+++ add_peers_dlgrec.php
@@ -149,57 +149,57 @@ input[type=number] {
                                                             </tr>
 
                                                             <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;width: 10em;">時間</td>
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);">輔導內容:生活、輔導、職訓及相關事宜</td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">8:30<br/>9:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_0" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">9:30<br/>10:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_1" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">10:30<br/>11:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_2" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">11:30<br/>12:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_3" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">12:30<br/>13:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_4" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">13:30<br/>14:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_5" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">14:30<br/>15:30</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_6" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
-                                                                        <tr style="text-align:left">
-                                                                            <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">15:30<br/>18:00</td>
-                                                                            <td style="border-bottom: solid 1px;">
-                                                                            <textarea class="peers_dlgrec_question" id="peers_dlgrec_7" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
-                                                                            </td>
-                                                                        </tr>
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;width: 10em;">時間</td>
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);">輔導內容:生活、輔導、職訓及相關事宜</td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">8:30<br/>9:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_0" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">9:30<br/>10:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_1" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">10:30<br/>11:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_2" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">11:30<br/>12:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_3" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">12:30<br/>13:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_4" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">13:30<br/>14:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_5" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">14:30<br/>15:30</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_6" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="text-align:left">
+                                                                <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">15:30<br/>18:00</td>
+                                                                <td style="border-bottom: solid 1px;">
+                                                                <textarea id="peers_dlgrec_7" name="peers_dlgrec_textarea" placeholder="請輸入內容" contenteditable="true"></textarea>
+                                                                </td>
+                                                            </tr>
                                                             <tr style="text-align:left">
                                                                 <td style="text-align:center;background-color:rgb(255 201 54);border-bottom-color: white;border-right-color: white;">特殊個案反應情形輔導處理</td>
                                                                 <td style="border-bottom: solid 1px;">
